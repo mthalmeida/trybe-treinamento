6f5da54aeb175e58c9a3375556c6cf935f73bf55
Matheus Almeida <matheus.almeida.neves@gmail.com>
2022-07-20 18:59:27 -0300

    Adiciona operadores aritimeticos

---
+++ 01-fundamentos/04-introducao-javascript-e-logica-de-programacao/01a-introducao-javascript/exercicios.js
@@ -0,0 +1,22 @@
+let primeiroNumero = 20
+let segundoNumero = 30
+let terceiroNumero = 45
+
+//adição
+const adicao = primeiroNumero + segundoNumero
+
+//Subtração
+const subtracao = primeiroNumero - segundoNumero
+
+//Multiplicação
+const multipicacao = primeiroNumero * segundoNumero
+
+//Divisão
+const divisao = primeiroNumero / segundoNumero
+
+//Módulo
+const modulo = primeiroNumero % segundoNumero
+
+
+
+
